6ad571eb355947608135531d0c4e3c58c5f86648
wtopull <471601494@qq.com>
2018-07-24 19:19:56 +0800

    nathan

--- src/components/page/money/creditc.vue
+++ src/components/page/money/creditc.vue
@@ -1,12 +1,13 @@
-<template lang="jade">
-.viewBox
-  .userRight
-    .userTitle 我要充值
-    .userMain
-      newTab(:navNum=6)
-      .maintain(style='padding-top: 180px;padding-bottom: 180px;text-align:center;')
-        i(class='iconfont hf-setting-fill')
-        span 信用卡支付维护中，请选择其它充值方式
+<template>
+<div class="viewBox">
+  <div class="userRight">
+    <div class="userTitle">我要充值</div>
+    <div class="userMain">
+      <newTab :navNum="6"></newTab>
+      <div style="padding-top: 180px;padding-bottom: 180px;text-align:center;" class="maintain"><i class="iconfont hf-setting-fill"></i><span>信用卡支付维护中，请选择其它充值方式</span></div>
+    </div>
+  </div>
+</div>
 </template>
 <script>
 import { baseUrl } from "../../../assets/js/env";
