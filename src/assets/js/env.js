445bbea22713405fc5244e8acc5e07336c007e10
john <wangyigma@gmail.com>
2018-09-18 16:16:03 +0800

    john

--- src/assets/js/env.js
+++ src/assets/js/env.js
@@ -2,11 +2,25 @@
 // baseUrl: 域名地址
 // routerMode: 路由模式
 
+
+// if (process.env.NODE_ENV == 'development') {
+//   baseUrl = 'http://10.25.8.171:8088';
+// } else if (process.env.NODE_ENV == 'production') {
+//   baseUrl = 'http://edu0370.com';
+// }
 let baseUrl = '';
-if (process.env.NODE_ENV == 'development') {
-  baseUrl = 'http://edu0370.com';
-} else if (process.env.NODE_ENV == 'production') {
-  baseUrl = 'http://edu0370.com';
+//test & prod
+if (process.env.NODE_ENV === 'production') {
+  if (location.hostname === 'qt.edu0370.com')
+    baseUrl = 'http://' + location.hostname.replace('m.', '') + '/';
+  else
+    baseUrl = 'https://' + location.hostname.replace('m.', '') + '/';
+  if (location.hostname === '10.25.8.171')
+    baseUrl = 'http://' + location.hostname + ':8088/';
+}
+//local
+else {
+  baseUrl = 'http://edu0370.com/';
 }
 export {
   baseUrl
